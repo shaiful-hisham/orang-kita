096af97e8162ffd249ad35e7a96b3e16771aba94
shaiful <shaiful.hisham@outlook.com>
2017-09-29 11:52:40 +0800

    Initial commit

---
+++ src/com/orangkita/client/Login.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.orangkita.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+
+/**
+ * @author Shaiful Hisham Mat Jali
+ *
+ */
+
+public class Login implements EntryPoint {
+	
+	@Override
+	public void onModuleLoad() {
+		
+		
+		
+	}
+
+}
